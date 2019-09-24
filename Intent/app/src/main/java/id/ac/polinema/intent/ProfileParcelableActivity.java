f4c6ad889556f8d0d1bfe857aedd0c374bb07921
GideonMei <mei.gideon.gm@gmail.com>
2019-09-24 17:21:55 +0700

    Membuat Program pada Parcelabel (Menampilkan Data secara paket)

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,20 +3,40 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.User;
+import static id.ac.polinema.intent.ParcelableActivity.USER_KEY;
+
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_parcelable);
-
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
             // TODO: display value here
+            User use = extras.getParcelable(USER_KEY);
+
+            String username =use.getUsername();
+            String name =use.getName();
+            int age =use.getAge();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
